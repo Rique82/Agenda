5b92d9e421d3a0083cc05e16fc1cb1a2fecaf81c
Henrique <henriqueeliashespanhol@gmail.com>
2025-11-28 22:12:26 -0300

    databaseatt

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,11 +7,11 @@ class Database {
 
     init(){
         this.db = new Sequelize({
-            database: process.env.DBname ?? 'agenda_u0c9',
-            host: process.env.DBHost ?? 'dpg-d4l3eoali9vc73e2o090-a',
-            username: process.env.DBusername ?? 'agenda_u0c9_user',
-            password: process.env.DBpassword  ?? 'r8i93ponSlVdPJ8RF1fOD1rA2jPia98f',
-            dialect: process.env.Dialect  ?? 'postgres'
+            database:'agenda_u0c9',
+            host:'dpg-d4l3eoali9vc73e2o090-a',
+            username:'agenda_u0c9_user',
+            password:'r8i93ponSlVdPJ8RF1fOD1rA2jPia98f',
+            dialect:'postgres'
         })
     }
 }
